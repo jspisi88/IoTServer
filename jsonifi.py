023b8d14cfe370f132b59bc9067fd399882f2228
Josip <jspisi88@gmail.com>
2021-02-24 19:42:37 +0100

    323

--- jsonifi.py
+++ jsonifi.py
@@ -20,7 +20,7 @@ def  hello_world():
     return "hello world"
  
 
-@app.route("/api/books", methods=['GET'])
+@app.route("blabalb", methods=['GET'])
 def return_all():
     conn = mysql.connect()
     cursor = conn.cursor()
@@ -30,7 +30,7 @@ def return_all():
  
     return jsonify({'rows': rows})
  
-@app.route("/api/books/titles", methods=['GET'])
+@app.route("blabalb", methods=['GET'])
 def return_titles():
     titles = []
     for book in books:
